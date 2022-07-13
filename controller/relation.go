0edb6378af5794314d5b2556a0b7525f87626676
win_kuan <kuan_525@163.com>
2022-07-13 17:30:14 +0800

    update controller relation

--- controller/relation.go
+++ controller/relation.go
@@ -1,8 +1,10 @@
 package controller
 
 import (
+	"github.com/FIFCOM/go-tiktok-lite/service"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 type UserListResponse struct {
@@ -12,12 +14,25 @@ type UserListResponse struct {
 	UserList []User `json:"user_list"`
 }
 
-// RelationAction 没有实际作用，仅仅检查token是否有效
+// RelationAction 关注或者取消关注操作
 func RelationAction(c *gin.Context) {
+	svcR := service.RelationSvc{}
+	// 用户token
 	token := c.Query("token")
+	// 当前用户Id
+	userIdString := c.Query("user_id")
+	userId, _ := strconv.ParseInt(userIdString, 10, 64)
+	// 对方用户Id
+	toUserIdString := c.Query("to_user_Id")
+	toUserId, _ := strconv.ParseInt(toUserIdString, 10, 64)
+	// 操作状态码（关注/取关）
+	actionTypeString := c.Query("action_type")
+	actionType, _ := strconv.ParseInt(actionTypeString, 10, 64)
 
 	// 判断当前的用户序列是否存在
 	if _, exist := usersLoginInfo[token]; exist {
+		// 根据actionType来选择关注或者取关
+		svcR.RelationAction(userId, toUserId, actionType)
 		c.JSON(http.StatusOK, Response{StatusCode: 0})
 	} else {
 		c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "User doesn't exist"})
@@ -26,23 +41,72 @@ func RelationAction(c *gin.Context) {
 
 // FollowList 返回一个响应列表
 func FollowList(c *gin.Context) {
+	svcR := service.RelationSvc{}
+	svcU := service.UserSvc{}
+
+	// 取得当前用户的id
+	userIdString := c.Query("user_id")
+	userId, _ := strconv.ParseInt(userIdString, 10, 64)
+
+	// 得到当前id的所有关注列表
+	follow := svcR.GetUserFans(userId)
+	var userList []User
+
+	// 将follow 映射 成为User结构体
+	for i := 0; i < len(follow); i++ {
+		// 将 follow[i] 转为User，命名为tmp
+		userId := follow[i].UserId
+		tmp := User{
+			Id:            userId,
+			Name:          svcU.GetUserById(userId).Name,
+			FollowCount:   svcR.LenUserFocus(userId),
+			FollowerCount: svcR.LenUserFans(userId),
+			IsFollow:      true,
+		}
+		userList = append(userList, tmp)
+	}
 
 	c.JSON(http.StatusOK, UserListResponse{
 		Response: Response{
 			StatusCode: 0,
 		},
-		// DemoUser 是一个User的demo
-		UserList: []User{DemoUser},
+		// 将关注列表返回
+		UserList: userList,
 	})
 }
 
 // FollowerList 返回一个响应列表
 func FollowerList(c *gin.Context) {
+	svcR := service.RelationSvc{}
+	svcU := service.UserSvc{}
+
+	// 取得当前用户的id
+	userIdString := c.Query("user_id")
+	userId, _ := strconv.ParseInt(userIdString, 10, 64)
+
+	// 得到当前id的所有关注列表
+	follower := svcR.GetUserFans(userId)
+	var userList []User
+
+	// 将follower 映射 成为User结构体
+	for i := 0; i < len(follower); i++ {
+		// 将 follower[i] 转为User，命名为tmp
+		userId := follower[i].UserId
+		tmp := User{
+			Id:            userId,
+			Name:          svcU.GetUserById(userId).Name,
+			FollowCount:   svcR.LenUserFocus(userId),
+			FollowerCount: svcR.LenUserFans(userId),
+			IsFollow:      true,
+		}
+		userList = append(userList, tmp)
+	}
+
 	c.JSON(http.StatusOK, UserListResponse{
 		Response: Response{
 			StatusCode: 0,
 		},
-		// DemoUser 是一个User的demo
-		UserList: []User{DemoUser},
+		// 将粉丝列表返回
+		UserList: userList,
 	})
 }
--- service/relation.go
+++ service/relation.go
@@ -5,26 +5,31 @@ import "github.com/FIFCOM/go-tiktok-lite/dao"
 type RelationSvc struct {
 }
 
+// GetUserFocus 根据id得到用户的关注列表
 func (us *RelationSvc) GetUserFocus(id int64) []dao.Follow {
 	follow, _ := dao.GetUserFocus(id)
 	return follow
 }
 
+// GetUserFans 根据id得到用户的粉丝列表
 func (us *RelationSvc) GetUserFans(id int64) []dao.Follower {
 	follower, _ := dao.GetUserFans(id)
 	return follower
 }
 
-func (us *RelationSvc) LenUserFocus(id int64) int {
+// LenUserFocus 根据id得到用户的关注数量
+func (us *RelationSvc) LenUserFocus(id int64) int64 {
 	follow, _ := dao.GetUserFocus(id)
-	return len(follow)
+	return int64(len(follow))
 }
 
-func (us *RelationSvc) LenUserFans(id int64) int {
+// LenUserFans 根据id得到用户的粉丝数量
+func (us *RelationSvc) LenUserFans(id int64) int64 {
 	follower, _ := dao.GetUserFans(id)
-	return len(follower)
+	return int64(len(follower))
 }
 
+// RelationAction 关注或取消关注操作，传入当前用户，目标用户，操作类型
 func (us *RelationSvc) RelationAction(userId int64, toUserId int64, actionType int64) {
 	follow := dao.Follow{
 		UserId:  userId,
