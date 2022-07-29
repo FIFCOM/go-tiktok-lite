4553e87619a1c1b9df661a56113a278074128971
cyzh0173 <1487761808@qq.com>
2022-07-29 23:52:34 +0800

    Create comment.go
    新建comment svc 层，确定了2个函数的基本类型，为编写提供了条件

---
+++ service/comment.go
@@ -0,0 +1,12 @@
+package service
+
+type CommentSvc struct {
+}
+
+func (Cs *CommentSvc) CommentAction(userId int64, videoId int64, actionType int32, commentText string, commentId int64) {
+
+}
+
+func (Cs *CommentSvc) CommentList(videoId int64) {
+
+}
